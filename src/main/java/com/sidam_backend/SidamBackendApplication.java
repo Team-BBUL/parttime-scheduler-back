f5e1456b8f3424e61085ffa2c1fd9ef647fa01e9
qqOwOqq <skdnfl4343@gmail.com>
2023-04-12 16:30:00 +0900

    first commit

---
+++ src/main/java/com/sidam_backend/SidamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sidam_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SidamBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SidamBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sidam_backend/SidamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sidam_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidamBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
