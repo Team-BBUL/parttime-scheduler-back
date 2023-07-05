3c97dd18151d448d2e358fdb54d8a6822782a676
seonu <ensony713@gmail.com>
2023-07-05 22:06:41 +0900

    07/05 공지사항 완료 - 테스트
    # Conflicts:
    #	src/main/java/com/sidam_backend/controller/NoticeController.java
    #	src/main/java/com/sidam_backend/data/Notice.java
    #	src/main/java/com/sidam_backend/repo/ImageFileRepository.java
    #	src/main/java/com/sidam_backend/repo/NoticeRepository.java
    #	src/main/java/com/sidam_backend/resources/GetNoticeList.java
    #	src/main/java/com/sidam_backend/resources/PostNotice.java
    #	src/main/java/com/sidam_backend/service/NoticeService.java

--- src/main/java/com/sidam_backend/repo/NoticeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sidam_backend.repo;
-
-import com.sidam_backend.data.Notice;
-import org.springframework.data.repository.CrudRepository;
-
-public interface NoticeRepository extends CrudRepository<Notice, Long> {
-}
