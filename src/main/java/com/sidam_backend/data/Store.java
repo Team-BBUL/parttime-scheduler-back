3558a1044dd9451dd3dc5ef1c2c9d5f9ef98668e
qqOwOqq <skdnfl4343@gmail.com>
2023-08-03 12:23:27 +0900

    엔티티 변수명 수정

--- src/main/java/com/sidam_backend/data/Store.java
+++ src/main/java/com/sidam_backend/data/Store.java
@@ -1,9 +1,12 @@
 package com.sidam_backend.data;
 
+import com.sidam_backend.data.enums.Role;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serializable;
 import java.util.List;
@@ -13,36 +16,39 @@ import java.util.List;
 @Table(name="store")
 public class Store implements Serializable {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @NotBlank
+    @Column(unique = true)
     private String name;
 
-    @NotBlank
+//    @NotBlank
     private String location;
 
-    @NotBlank
+//    @NotBlank
     private String phone;
 
-    @NotBlank
+//    @NotBlank
     private int open;
     // hh 형식
 
-    @NotBlank
+//    @NotBlank
     private int close;
     // hh 형식
 
     @NotNull
     private int idx;
 
+//    @NotNull
+    private int payday;
     // 근무불가능시간 선택 마감일 = 주차 시작일로부터 n일 전까지
-    @NotNull
+//    @NotNull
     private int deadline;
 
     // 주차 시작일 = 월 화 수 목 금 토 일
     //             1  2  3 4  5  6 7
-    @NotNull
-    private int week;
+//    @NotNull
+    private int weekStartDay;
+
 }
