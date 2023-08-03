b835f0ce8f293ff9b441c673676b1d960ca6cb6f
qqOwOqq <skdnfl4343@gmail.com>
2023-08-03 12:13:28 +0900

    id, oauth2 조회 쿼리 추가 존재 여부 확인 쿼리 추가

--- src/main/java/com/sidam_backend/repo/AccountRepository.java
+++ src/main/java/com/sidam_backend/repo/AccountRepository.java
@@ -6,5 +6,10 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Optional;
 
 public interface AccountRepository extends CrudRepository<Account, String> {
+    Optional<Account> findById(Long id);
     Optional<Account> findByEmail(String email);
+    Optional<Account> findByOauth2Id(Long oauth2Id);
+    boolean existsByIdAndNameIsNotNull(Long id);
+
+
 }
