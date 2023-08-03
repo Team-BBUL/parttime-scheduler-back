2858c9daac4b416524d35fbff511e553e0ce172c
qqOwOqq <skdnfl4343@gmail.com>
2023-08-03 12:25:57 +0900

    이름으로 조회, 확인 기능 추가

--- src/main/java/com/sidam_backend/repo/StoreRepository.java
+++ src/main/java/com/sidam_backend/repo/StoreRepository.java
@@ -4,4 +4,7 @@ import com.sidam_backend.data.Store;
 import org.springframework.data.repository.CrudRepository;
 
 public interface StoreRepository extends CrudRepository<Store, Long> {
+
+    Store findByName(String name);
+    boolean existsByName(String name);
 }
