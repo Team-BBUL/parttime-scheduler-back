508c7afb17149943dc5e951ab9b3670fe3910313
qqOwOqq <skdnfl4343@gmail.com>
2023-08-03 12:27:26 +0900

    중복 매장명 검증 기능 구현

---
+++ src/main/java/com/sidam_backend/validator/StoreValidator.java
@@ -0,0 +1,30 @@
+package com.sidam_backend.validator;
+
+import com.sidam_backend.repo.StoreRepository;
+import com.sidam_backend.resources.StoreForm;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class StoreValidator implements Validator {
+
+    private final StoreRepository storeRepository;
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return clazz.isAssignableFrom(StoreForm.class);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        StoreForm storeForm = (StoreForm) target;
+        if(storeRepository.existsByName(storeForm.getName())){
+            errors.rejectValue("name", "name_duplicated", "스토어 이름이 중복되었습니다");
+        }
+    }
+}
