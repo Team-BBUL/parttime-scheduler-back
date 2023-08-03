e20214b97832af6360ea456fdee58800b1a58886
qqOwOqq <skdnfl4343@gmail.com>
2023-08-03 12:19:26 +0900

    시큐리티 필터 구현

--- src/main/java/com/sidam_backend/security/SecurityConfig.java
+++ src/main/java/com/sidam_backend/security/SecurityConfig.java
@@ -1,11 +1,15 @@
 package com.sidam_backend.security;
 
+import com.sidam_backend.service.CustomOAuth2UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 import org.springframework.web.filter.CorsFilter;
@@ -28,9 +32,21 @@ public class SecurityConfig {
     }
 
     @Bean
+    public AuthenticationEntryPoint authenticationEntryPoint(){
+        return (request, response, e) ->{
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.setContentType("text/plain;charset=UTF-8");
+            response.getWriter().write("UNAUTHORIZED");
+            response.getWriter().flush();
+            response.getWriter().close();
+        };
+    }
+
+    @Bean
     public BCryptPasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
     }
+
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
@@ -41,7 +57,7 @@ public class SecurityConfig {
                 .formLogin().disable()
                 .httpBasic().disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/","/auth/** ","/auth2/**").permitAll()
+                .requestMatchers("/","/auth/** ","/auth2/**", "/oauth","/login","/sociallogin*").permitAll()
                 .requestMatchers("/api").permitAll()
                 .anyRequest().authenticated()
                 .and()
@@ -57,13 +73,12 @@ public class SecurityConfig {
                 .successHandler(oAuthSuccessHandler)
                 .and()
                 .exceptionHandling()
-                .authenticationEntryPoint(new Http403ForbiddenEntryPoint());
+                .authenticationEntryPoint(authenticationEntryPoint());
 
         http.addFilterAfter(
                 jwtAuthenticationFilter,
                 CorsFilter.class
         );
-
 
         return http.build();
     }
