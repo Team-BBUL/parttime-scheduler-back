9eb92eb59819b8701a83f6e6589cb31f671ae901
qqOwOqq <skdnfl4343@gmail.com>
2023-08-16 17:51:14 +0900

    인센티브 entity 추가

---
+++ src/main/java/com/sidam_backend/data/Incentive.java
@@ -0,0 +1,15 @@
+package com.sidam_backend.data;
+
+import java.time.LocalDateTime;
+
+public class Incentive {
+
+    private Long id;
+
+    private int cost;
+
+    private String description;
+
+    private LocalDateTime date;
+
+}
