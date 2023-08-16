637a231763f47e577596ebf578c8b029d7ddb691
qqOwOqq <skdnfl4343@gmail.com>
2023-08-16 17:41:20 +0900

    메소드 재사용성 리팩토링

--- src/main/java/com/sidam_backend/service/AccountService.java
+++ src/main/java/com/sidam_backend/service/AccountService.java
@@ -16,11 +16,8 @@ public class AccountService {
     private final AccountRepository accountRepository;
 
     public void saveAccount(Long id, Map<String, Object> resultMap){
-
-        Account account = accountRepository.findById(id)
-                .orElseThrow(()-> new IllegalArgumentException(id + "에 해당하는 계정이 없습니다."));
+        Account account =  this.getAccount(id);
         account.setName((String) resultMap.get("name"));
-        account.setOnceVerified(true);
         accountRepository.save(account);
     }
 
