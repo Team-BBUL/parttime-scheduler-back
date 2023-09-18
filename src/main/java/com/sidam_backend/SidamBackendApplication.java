223b1d258292462f8394940ce44ffcf7f5a3d6b9
seon <ensony713@gmail.com>
2023-09-18 16:51:59 +0900

    서버 배포용 수정

--- src/main/java/com/sidam_backend/SidamBackendApplication.java
+++ src/main/java/com/sidam_backend/SidamBackendApplication.java
@@ -2,10 +2,18 @@ package com.sidam_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SidamBackendApplication {
+public class SidamBackendApplication extends SpringBootServletInitializer {
+
 	public static void main(String[] args) {
 		SpringApplication.run(SidamBackendApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SidamBackendApplication.class);
 	}
 }
